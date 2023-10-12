6caa3123289534419c3fea0b74bac8f6541797fe
santiagor06 <santiago.restrepo06@hotmail.com>
2023-10-11 21:02:39 -0500

    prueba

--- assets/js/functions/prueba.js
+++ assets/js/functions/prueba.js
@@ -1,1 +1,1 @@
-export const prueba =()=>console.log("Esto es una funcion de prueba,para usar js module");
+export const prueba =()=>console.log("Esto es una funcion de prueba,para usadedr js module");
