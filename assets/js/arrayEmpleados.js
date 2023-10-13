da3d368b7ece663958e0ed2ca4dec097c71aa16d
santiagor06 <santiago.restrepo06@hotmail.com>
2023-10-12 22:38:44 -0500

    maquetado cards

---
+++ assets/js/arrayEmpleados.js
@@ -0,0 +1,38 @@
+export const empleados=[
+    {
+    nombre: "Juan",
+    apellido: "Pérez",
+    cargo: "Desarrollador",
+    fechaIngreso: "2023-01-01",
+    imagen: "ruta/de/la/imagen1.jpg",
+    historialSalarios: [
+        {cantidad: 30000, fecha: "2023-01-01"},
+        {cantidad: 35000, fecha: "2023-02-01"},
+        {cantidad: 38000, fecha: "2023-03-01"}
+    ]
+},
+    {
+    nombre: "rodrigo",
+    apellido: "Pérez",
+    cargo: "Desarrollador",
+    fechaIngreso: "2023-01-01",
+    imagen: "ruta/de/la/imagen1.jpg",
+    historialSalarios: [
+        {cantidad: 30000, fecha: "2023-01-01"},
+        {cantidad: 35000, fecha: "2023-02-01"},
+        {cantidad: 38000, fecha: "2023-03-01"}
+    ]
+},
+    {
+    nombre: "Juan",
+    apellido: "Pérez",
+    cargo: "Desarrollador",
+    fechaIngreso: "2023-01-01",
+    imagen: "ruta/de/la/imagen1.jpg",
+    historialSalarios: [
+        {cantidad: 30000, fecha: "2023-01-01"},
+        {cantidad: 35000, fecha: "2023-02-01"},
+        {cantidad: 38000, fecha: "2023-03-01"}
+    ]
+},
+];
