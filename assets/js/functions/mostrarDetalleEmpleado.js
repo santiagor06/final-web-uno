e0ade4b7caa64e0f6f9169abce59bc6a983b18c0
leruizr <leruizres1986mail.com>
2023-10-17 15:43:37 -0500

    cambios detail

--- assets/js/functions/mostrarDetalleEmpleado.js
+++ assets/js/functions/mostrarDetalleEmpleado.js
@@ -1,13 +1,38 @@
-import { setModal } from "./setModal.js"
+import { empleados } from "../arrayEmpleados.js";
+import { setModal } from "./setModal.js";
+import { volverMayuscula } from "./volverMayuscula.js";
 
-export const mostrarDetalleEmpleado=()=>{
-    setModal()
-     const prueba=document.createElement("h1")
-     prueba.append("DETAIL")
-     const container=document.querySelector("#container-dialog")
-     const hijo=container.firstChild;
-     if(hijo)container.removeChild(hijo)
-    container.append(prueba)
+export const mostrarDetalleEmpleado = (empleado) => {
+    setModal();
+
+    const container = document.querySelector("#container-dialog");
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+
+    const detalleContainer = document.createElement("div");
+    detalleContainer.classList.add("detalle-container");
+
+    const nombreCompleto = document.createElement("h2");
+    nombreCompleto.textContent = `${volverMayuscula(empleados.nombre)} ${volverMayuscula(empleados.apellido)}`;
+
+    const imagenEmpleado = document.createElement("img");
+    imagenEmpleado.src = empleados.imagen;
+    imagenEmpleado.classList.add("imagen-empleado");
+
+    const historialPagos = document.createElement("div");
+    historialPagos.classList.add("historial-pagos");
+    historialPagos.textContent = "Historial de Pagos:";
     
+    empleados.historialSalarios.forEach((pago) => {
+        const pagoItem = document.createElement("p");
+        pagoItem.textContent = `Fecha: ${pago.fecha}, Cantidad: $${pago.cantidad}`;
+        historialPagos.appendChild(pagoItem);
+    });
 
-}
+    detalleContainer.appendChild(nombreCompleto);
+    detalleContainer.appendChild(imagenEmpleado);
+    detalleContainer.appendChild(historialPagos);
+
+    container.appendChild(detalleContainer);
+};
